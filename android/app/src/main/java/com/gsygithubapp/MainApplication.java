39c00b495dabf14973820f1609c1405b2bd4633d
Shuyu Guo <359369982@qq.com>
2017-11-09 22:13:09 +0800

    add "react-native-modalbox" "react-native-spinkit" "react-native-vector-icons" 2017-10-09

--- android/app/src/main/java/com/gsygithubapp/MainApplication.java
+++ android/app/src/main/java/com/gsygithubapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.gsygithubapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.react.rnspinkit.RNSpinkitPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNSpinkitPackage(),
             new RNI18nPackage()
       );
     }
