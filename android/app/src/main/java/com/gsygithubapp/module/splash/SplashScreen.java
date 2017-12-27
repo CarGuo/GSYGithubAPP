9f650ebdf3594eebdb2206d90e0945a51970c7e9
guoshuyu <guoshuyu@ygsoft.com>
2017-12-27 16:33:26 +0800

    优化android启动

---
+++ android/app/src/main/java/com/gsygithubapp/module/splash/SplashScreen.java
@@ -0,0 +1,80 @@
+package com.gsygithubapp.module.splash;
+
+import android.app.Activity;
+import android.app.Dialog;
+
+import com.gsygithubapp.R;
+
+import java.lang.ref.WeakReference;
+
+
+public class SplashScreen {
+    private static int NULL_ID = 0;
+    private static Dialog mSplashDialog;
+    private static WeakReference<Activity> mActivity;
+
+    /**
+     * 打开启动屏
+     */
+    public static void show(final Activity activity, final boolean fullScreen, final int themeResId) {
+        if (activity == null) return;
+        mActivity = new WeakReference<Activity>(activity);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (!activity.isFinishing()) {
+
+                    mSplashDialog = new Dialog(
+                            activity,
+                            themeResId != NULL_ID ? themeResId
+                                    : fullScreen ? R.style.SplashScreen_Fullscreen
+                                    : R.style.SplashScreen_SplashTheme
+                    );
+                    mSplashDialog.setContentView(R.layout.splash);
+                    mSplashDialog.setCancelable(false);
+
+                    if (!mSplashDialog.isShowing()) {
+                        mSplashDialog.show();
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * 打开启动屏
+     */
+    public static void show(final Activity activity, final boolean fullScreen) {
+        show(activity, fullScreen, 0);
+    }
+
+    /**
+     * 打开启动屏
+     */
+    public static void show(final Activity activity) {
+        show(activity, false);
+    }
+
+    /**
+     * 关闭启动屏
+     */
+    public static void hide(Activity activity) {
+        if (activity == null) {
+            if (mActivity == null) {
+                return;
+            }
+            activity = mActivity.get();
+        }
+        if (activity == null) return;
+
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mSplashDialog != null && mSplashDialog.isShowing()) {
+                    mSplashDialog.dismiss();
+                    mSplashDialog = null;
+                }
+            }
+        });
+    }
+}
---
+++ android/app/src/main/java/com/gsygithubapp/module/splash/SplashScreenModule.java
@@ -0,0 +1,40 @@
+package com.gsygithubapp.module.splash;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+/**
+ * SplashScreen
+ * 启动屏
+ * from：http://www.devio.org
+ * Author:CrazyCodeBoy
+ * GitHub:https://github.com/crazycodeboy
+ * Email:crazycodeboy@gmail.com
+ */
+public class SplashScreenModule extends ReactContextBaseJavaModule{
+    public SplashScreenModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "SplashScreen";
+    }
+
+    /**
+     * 打开启动屏
+     */
+    @ReactMethod
+    public void show() {
+        SplashScreen.show(getCurrentActivity());
+    }
+
+    /**
+     * 关闭启动屏
+     */
+    @ReactMethod
+    public void hide() {
+        SplashScreen.hide(getCurrentActivity());
+    }
+}
