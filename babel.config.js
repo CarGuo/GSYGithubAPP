b4642d91789b5759a2d005946c4e992f48e2da4c
guoshuyu <359369982@qq.com>
2019-04-17 10:48:03 +0800

    修改 babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [
-        ["@babel/plugin-proposal-decorators", {"legacy": true}],
-        ["@babel/plugin-proposal-class-properties", {"loose": true}]]
+        ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+        ['import', { libraryName: '@ant-design/react-native' }] // 与 Web 平台的区别是不需要设置 style
+    ]
 };
